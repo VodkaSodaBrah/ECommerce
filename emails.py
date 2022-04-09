23889f215760a4fb52f7210735f33716376ff9c7
Michael Childress <mchildress@me.com>
2022-04-08 22:37:23 -0400

    added static directory, products section (add, get, delete), finished api functionality

--- emails.py
+++ emails.py
@@ -1,15 +1,14 @@
-from email import message
-from re import template
-from fastapi import (BackgroundTasks, UploadFile, File, Form, Depends, HTTPException, status)
-from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from fastapi import (BackgroundTasks, UploadFile, 
+                    File, Form, Depends, HTTPException, status)
+
 from dotenv import dotenv_values
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import List
-from .models import User
+from fastapi_mail import FastMail, MessageSchema,ConnectionConfig
 import jwt
+from models import User
 
-config_credentials = dotenv_values(".env")
-
+config_credentials = dict(dotenv_values(".env"))
 conf = ConnectionConfig(
     MAIL_USERNAME = config_credentials["EMAIL"],
     MAIL_PASSWORD = config_credentials["PASS"],
@@ -24,43 +23,43 @@ conf = ConnectionConfig(
 class EmailSchema(BaseModel):
     email: List[EmailStr]
 
-async def send_email(email: List, instance: User):
+async def send_email(email : list, instance: User):
 
     token_data = {
         "id" : instance.id,
-        "username" : instance.username,
+        "username" : instance.username
     }
 
-    token = jwt.encode(token_data, config_credentials["SECRET"], algorithm='HS256') 
+    token = jwt.encode(token_data, config_credentials["SECRET"])
 
     template = f"""
         <!DOCTYPE html>
         <html>
-            <head>
-
-            </head>
-            <body>
-                <div style = "display: flex; align-items: center; justify-content: center; flex-direction: column">
-
-                    <h3>Account Verification</h3>
-                    <br>
-
-                    <p>Thanks for choosing "insert name of business, please click on the button below to verify your account</p>
-
-                    <a style="margin-top: 1rem; padding: 1rem; border-radius: 0.5rem; dont-size: 1rem; text-decoration: none; background: #0275d8; color: white" href="https://localhost:8000/verification/?token={token}">Verify your email</a>
-
-                    <p>Please ignore this email if you did not register for "NAME""</p>
-                </div>
-            </body>
+        <head>
+        </head>
+        <body>
+            <div style=" display: flex; align-items: center; justify-content: center; flex-direction: column;">
+                <h3> Account Verification </h3>
+                <br>
+                <p>Thanks for choosing EasyShopas, please 
+                click on the link below to verify your account</p> 
+                <a style="margin-top:1rem; padding: 1rem; border-radius: 0.5rem; font-size: 1rem; text-decoration: none; background: #0275d8; color: white;"
+                 href="http://localhost:8000/verification/?token={token}">
+                    Verify your email
+                <a>
+                <p style="margin-top:1rem;">If you did not register for EasyShopas, 
+                please kindly ignore this email and nothing will happen. Thanks<p>
+            </div>
+        </body>
         </html>
     """
 
     message = MessageSchema(
-        subject = "NAME Account Verification Email",
-        recipients = email, 
-        body = template,
-        subtype = "html"
-    )
+        subject="EasyShopas Account Verification Mail",
+        recipients=email,  # List of recipients, as many as you can pass 
+        body=template,
+        subtype="html"
+        )
 
     fm = FastMail(conf)
-    await fm.send_message(message=message)
+    await fm.send_message(message)
